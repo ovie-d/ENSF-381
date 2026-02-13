661e77e6a475ed87425b0d6f9c34b5dc21548ea9
David <david.onofeghara@gmail.com>
2026-02-13 12:40:58 -0700

    exercise3

---
+++ lab-03/Lab03/exercise3.js
@@ -0,0 +1,8 @@
+
+let ClassRoster = ["Alice", "Tom", "Charlie", "Diana", "Evan"];
+console.log(ClassRoster.toString());
+ClassRoster.push("Fiona", "Nancy");
+ClassRoster.shift();
+console.log(ClassRoster);
+let length = ClassRoster.length;
+console.log(length);
