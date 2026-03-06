876ff10d39541bf8adb1b582ecffe698cddbe90b
David <david.onofeghara@gmail.com>
2026-03-06 11:49:41 -0700

    lab-05

---
+++ lab-05/jsx-exercise/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  const currentYear = new Date().getFullYear();
+  const isLoggedIn = true;
+
+  return (
+    <div className="App">
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>The current year is {currentYear}.</p>
+      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
+    </div>
+  );
+}
+export default App;
+
