f081e5cb7fc02e9857abceeccfc5e43012adcdbb
UnasKhalid <96748553+UnasKhalid@users.noreply.github.com>
2026-03-06 12:07:35 -0700

    Add login conditional rendering in App component

--- lab-05/jsx-exercise/src/App.js
+++ lab-05/jsx-exercise/src/App.js
@@ -3,8 +3,6 @@ import Controls from "./Controls";
 import UserList from "./UserList";
 import Footer from "./Footer";
 
-import './App.css';
-
 function App() {
   const currentYear = new Date().getFullYear();
   const isLoggedIn = true;
@@ -14,9 +12,25 @@ function App() {
       <h1>ENSF-381: Full Stack Web Development</h1>
       <p>React Components</p>
       <p>The current year is {currentYear}.</p>
-      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
+
+      {isLoggedIn ? (
+        <>
+          <p>Welcome back!</p>
+          <section>
+            <Controls />
+          </section>
+          <section>
+            <h2>User List</h2>
+            <UserList />
+          </section>
+          <Footer />
+        </>
+      ) : (
+        <p>Please log in.</p>
+      )}
+
     </div>
   );
 }
-export default App;
 
+export default App;
