34240195fc53d3a2527d1424436700f126a40143
David <david.onofeghara@gmail.com>
2026-03-29 22:20:32 -0600

    flavors

---
+++ Assignment 3/sweet-scoop-app/src/flavors.js
@@ -0,0 +1,91 @@
+const flavors = [
+{
+id: 1,
+name: "Vanilla Dream",
+price: "$4.99",
+description: " Classic creamy vanilla ice cream made with fresh ingredients.",
+duration: "8 weeks",
+image: "images/vanilla.jpg"
+},
+
+{
+id: 2,
+name: "Chocolate Bliss",
+price: "$5.99",     
+description: " Rich and indulgent chocolate ice cream for true chocolate lovers.",
+duration: "8 weeks",
+image: "images/chocolate.jpg"
+},
+
+{
+id: 3,
+name: "Strawberry Delight",
+price: "$5.49",
+description: " Sweet and refreshing strawberry ice cream made with real strawberries.",
+duration: "8 weeks",
+image: "images/strawberry.jpg"
+}
+
+{
+id: 4,
+name: "Minty Fresh",
+price: "$3.49",     
+description: " Cool and refreshing mint ice cream with a hint of chocolate chips.",
+duration: "8 weeks",
+image: "images/mint.jpg"    
+}
+
+{ id : 5,
+name: "Cookie Dough Craze",
+price: "$6.49",
+description: " Creamy cookie dough ice cream loaded with chunks of cookie dough.",
+duration: "8 weeks",
+image: "images/cookie_dough.jpg"    
+}
+
+{   
+id: 6,
+name: "Salted Caramel Swirl",
+price: "$6.99",
+description: " Sweet and salty caramel ice cream with a luscious caramel swirl.",
+duration: "8 weeks",
+image: "images/salted_caramel.jpg"
+}
+
+{
+id: 7,
+name: "Pistachio Paradise",
+price: "$5.99",
+description: " Nutty and creamy pistachio ice cream made with real pistachios.",
+duration: "8 weeks",
+image: "images/pistachio.jpg"
+}
+
+{   
+id: 8,
+name: "Coffee Buzz",
+price: "$5.99",
+description: " Bold and aromatic coffee ice cream for coffee enthusiasts.",
+duration: "8 weeks",
+image: "images/coffee.jpg"
+}
+
+{   
+id: 9,
+name: "Lemon Zest",
+price: "$6.49",
+description: " Tangy and refreshing lemon ice cream with a zesty kick.",
+duration: "8 weeks",
+image: "images/lemon.jpg"
+}
+
+{   
+id: 10,
+name: "Cookies and Cream",
+price: "$8.49",
+description: " Creamy vanilla ice cream loaded with chunks of chocolate cookies.",
+duration: "8 weeks",
+image: "images/cookies_and_cream.jpg"
+}
+];
+export default flavors;
